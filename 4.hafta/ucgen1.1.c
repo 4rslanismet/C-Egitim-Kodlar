c32c47cd82adba4e868c1c277700e16be0d18827
İSMET ARSLAN <50077308+4rslanismet@users.noreply.github.com>
2020-11-17 10:04:07 +0300

    Update ucgen1.1.c

--- 4.hafta/ucgen1.1.c
+++ 4.hafta/ucgen1.1.c
@@ -9,6 +9,12 @@ int main ()
 	printf("lütfen boyut giriniz");
 	scanf("%d",&yildiz);
        
+	if(yildiz<0)
+	{	
+	  printf("hatalı giriş yaptınız");
+	  return (1);
+	}
+	
 	for(i=0;i<yildiz;i++)
 	{
 	  for(j=0;j<=i;j++)
